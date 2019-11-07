36bc6f602f2d4d8969fcc5d62c1cd5c5c70bd848
maoguijun <maoguijun@mini1.cn>
2019-11-07 18:12:31 +0800

    commitizen

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: maoguiun
+ * @Date: 2019-11-07 18:00:49
+ * @LastEditors: maoguiun
+ * @LastEditTime: 2019-11-07 18:00:59
+ * @FilePath: \common-app\commitlint.config.js
+ */
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "refactor",
+                "test",
+                "chore",
+                "revert",
+            ],
+        ],
+        "subject-full-stop": [0, "never"],
+        "subject-case": [0, "never"],
+    },
+};
